bba623629552185e19076ee9d9236452d365131d
dokgo <tomasromeo1998@gmail.com>
2017-07-25 15:58:08 +0300

    Add test app to repo

---
+++ app/src/main/java/com/example/dokgo/chisw/HomeActivity.java
@@ -0,0 +1,48 @@
+package com.example.dokgo.chisw;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String COUNTER = "counter";
+    private TextView counterView;
+    private TextView emailView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        initViews();
+
+        SharedPreferences prefs =
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        int counter = prefs.getInt(COUNTER, -1);
+
+        counterView.setText(Integer.toString(counter));
+        emailView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("mailto:" + getString(R.string.email));
+                intent.setData(data);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initViews() {
+        counterView = (TextView) findViewById(R.id.counter_view);
+        emailView = (TextView) findViewById(R.id.email_view);
+    }
+}
---
+++ app/src/main/java/com/example/dokgo/chisw/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.dokgo.chisw;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIME = 3500;
+    private static final String COUNTER = "counter";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences prefs =
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        int counter = prefs.getInt(COUNTER, -1);
+        prefs.edit().putInt(COUNTER, ++counter).apply();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
